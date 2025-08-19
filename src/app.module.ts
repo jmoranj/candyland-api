76732d6356c69512f3a7d7e6ca4ccdea45a8bd00
jmoranj <morandim.joao@gmail.com>
2025-08-19 00:15:58 -0300

    🔧 fix: remove AuthModule from app imports to streamline module dependencies.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './modules/auth/auth.module';
+
 import { PrismaModule } from './modules/database/prisma.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
