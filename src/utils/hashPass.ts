97700403c4d45672845cc45585b517f4da8a7979
jmoranj <morandim.joao@gmail.com>
2025-08-25 19:19:45 -0300

    ✨ feat: update seed script to create admin user and multiple products

--- src/utils/hashPass.ts
+++ src/utils/hashPass.ts
@@ -1,7 +1,5 @@
-import * as bcrypt from 'bcryptjs';
 import * as crypto from 'crypto';
 
 export function generateRandomPassword(length = 12) {
   return crypto.randomBytes(length).toString('base64').slice(0, length);
 }
-
