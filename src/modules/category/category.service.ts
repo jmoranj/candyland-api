d13992cfe4e929d7b8cda5e436f5c0926b25a842
Gabriel Pauletti Egea <ga.paulettiegea@gmail.com>
2025-09-08 20:23:56 -0300

    ✨ feat(category): add category existence check in create method

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
 import { CategoriesDto } from './dto/category.dto';
 
@@ -7,6 +7,16 @@ export class CategoryService {
   constructor(private  readonly prisma: PrismaService) {}
 
   async create(data: CategoriesDto) {
+    const existingCategory = await this.prisma.category.findUnique({
+      where: {
+        categoryName: data.categoryName,
+      },
+    });
+
+    if (existingCategory) {
+      throw new BadRequestException('Categoria com esse nome já existe.');
+    }
+
     const category = await this.prisma.category.create({
       data: {
         categoryName: data.categoryName,
--- src/modules/database/prisma.service.ts
+++ src/modules/database/prisma.service.ts
@@ -1,9 +1,13 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
-    await this.$connect();
+    await this.$connect(); // conecta 1 vez só
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect(); // fecha conexão corretamente
   }
 }
