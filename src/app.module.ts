fb152c0f99822d7dccf657430dce6b41b5ba8d01
eduard0vieira <eduardohvieira05@gmail.com>
2025-10-07 00:43:12 -0300

    feaat: Add DashboardModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,17 @@ import { CategoryModule } from './modules/category/category.module';
 import { PrismaModule } from './modules/database/prisma.module';
 import { OrdersModule } from './modules/orders/order.module';
 import { ProductsModule } from './modules/products/product.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ProductsModule, OrdersModule, CategoryModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    ProductsModule,
+    OrdersModule,
+    CategoryModule,
+    DashboardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
